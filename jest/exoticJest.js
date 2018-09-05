4d2d3caa727926c585380140c3d9e2d85d51bf56
James <aretecode@gmail.com>
2018-09-05 14:40:38 -0700

    flow to ts for test-utils

--- jest/exoticJest.js
+++ jest/exoticJest.js
@@ -10,4 +10,4 @@ require('../test-utils/setupTestFramework.js')
 // Enzyme.configure({ adapter: new Adapter() })
 // // eslint-disable-next-line no-underscore-dangle
 // global.__SERVER__ = typeof document === 'undefined'
-// global.preval = x => eval(x)
+global.preval = x => eval(x)
