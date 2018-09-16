f8d32d2782838fae619d0fd86629c5ce5901aa91
James <aretecode@gmail.com>
2018-09-16 13:34:59 -0700

    babel & rollup using ts

--- .babelrc.js
+++ .babelrc.js
@@ -3,13 +3,19 @@ const modules = BABEL_ENV === 'cjs' || NODE_ENV === 'test' ? 'commonjs' : false
 
 const loose = true
 
+// @todo add `self` etc on dev build
 module.exports = {
-  presets: [['env', { loose, modules }], 'react'],
+  presets: [['@babel/preset-env', { loose, modules }]],
   plugins: [
-    'preval',
-    ['transform-react-remove-prop-types', { mode: 'unsafe-wrap' }],
-    'transform-object-rest-spread',
-    ['transform-class-properties', { loose }],
-    modules === 'commonjs' && 'add-module-exports',
-  ].filter(Boolean),
+    "@babel/plugin-syntax-typescript",
+    '@babel/plugin-transform-typescript',
+    '@babel/plugin-syntax-jsx',
+    '@babel/plugin-transform-react-jsx',
+
+    'babel-plugin-preval',
+    // ['transform-react-remove-prop-types', { mode: 'unsafe-wrap' }],
+    // ['transform-class-properties', { loose }],
+    // modules === 'commonjs' && 'add-module-exports',
+  ].map(require.resolve)
+  // .filter(Boolean),
 }
